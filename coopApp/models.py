6f1b5da4c1d9099c159643d6c041d111612d5337
matvey <matvey.karlov@inbox.ru>
2024-06-24 23:31:23 +0300

    SQLite test set up

--- coopApp/models.py
+++ coopApp/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Portfolio(models.Model):
+    name = models.CharField(max_length=20)
+    occupation = models.CharField(max_length=40)
