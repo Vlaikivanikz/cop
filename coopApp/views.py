f1da2ea2b6ca0d7d9b0a84dd5f32b47c4a5d7dbc
matvey <matvey.karlov@inbox.ru>
2024-06-24 22:28:45 +0300

    sync

--- coopApp/views.py
+++ coopApp/views.py
@@ -1,16 +1,4 @@
 from django.shortcuts import render
-from django import http as http
 
 def homepage(request):
-    code = f'''<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hellow</h1>
-</body>
-</html>'''
-    return http.HttpResponse()
+    return render(request, 'indexx.html')
