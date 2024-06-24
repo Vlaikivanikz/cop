890623c4458f0de69f0c684684bdeeb7d1b37697
Vlaikivanikz <listikvladik@gmail.com>
2024-06-24 21:33:57 +0300

    htnl new

--- coopApp/views.py
+++ coopApp/views.py
@@ -4,16 +4,9 @@ from django.shortcuts import render
 from datetime import datetime
 
 from django.http import HttpResponse
-
+from ccop import indexx
 
 def index(request):
     now = datetime.now()
-    html = f'''
-    <html>
-        <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
-        </body>
-    </html>
-    '''
+    html = indexx
     return HttpResponse(html)
