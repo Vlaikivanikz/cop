f489996260b3cb0d51c80d403dea93a74b14f1d4
matvey <matvey.karlov@inbox.ru>
2024-06-25 14:12:35 +0300

    db user api

--- coopApp/views.py
+++ coopApp/views.py
@@ -1,4 +1,26 @@
 from django.shortcuts import render
+from .models import Portfolio
+import asyncio
+import json
+# port = Portfolio.objects.create(name='HUi', occupation='Killer')
+# async def getPort():
+#     port = await Portfolio.objects.aget(id=1)
+#     print(port.name)
+# asyncio.run(getPort())
+ports = Portfolio.objects.all()
 
+
+def getPorts(req):
+    data = []
+    for p in ports:
+        obj = {}
+        obj['id'] = p.id
+        obj['name'] = p.name
+        obj['occupation'] = p.occupation
+        data.append(obj)
+    return render(req, json.dumps(data), content_type='text/plain')
+    
 def homepage(request):
     return render(request, 'indexx.html')
+def portfolios(req):
+    return render(req, 'portfolios.html')
