83a23d0c72556b82c010acfab5ab337c76d82c11
matvey <matvey.karlov@inbox.ru>
2024-07-14 19:59:44 +0300

    zaebali

--- ccop/settings.py
+++ ccop/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import os
 import dotenv
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,7 +29,7 @@ if os.path.isfile(dotenv_file):
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
-
+dotenv.load_dotenv[SECRET_KEY]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
