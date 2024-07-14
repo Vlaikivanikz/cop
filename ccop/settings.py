42a6b900ec75e2509092f7c31ec6c3f6b2c240d2
matvey <matvey.karlov@inbox.ru>
2024-07-14 19:49:08 +0300

    indus

--- ccop/settings.py
+++ ccop/settings.py
@@ -11,17 +11,22 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u819o1+i%)6=&*bdkt83yj(1(nhh!f6(6z95x2av)9nbb)lk#$'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -84,12 +89,12 @@ WSGI_APPLICATION = 'ccop.wsgi.app'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'django',
-        'USER': 'coop_admin',
-        'PASSWORD': 'password',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': os.environ("ENGINE"),
+        'HOST': os.environ("HOST"),
+        'NAME': os.environ("NAME"),
+        'USER': os.environ("USER"),
+        'PASSWORD': os.environ("PASSWORD"),
+        'HOST': os.environ("HOST"),
     }
 }
 
